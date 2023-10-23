15b69738b6d2220e00be361b9b504d9932538ef9
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2023-10-23 20:36:26 +0100

    hey

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -9,19 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class StudentController extends AbstractController
 {
     #[Route('/student', name: 'app_student')]
-    public function createProduct(EntityManagerInterface $entityManager): Response
+    public function index(): Response
     {
-        $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setDescription('Ergonomic and stylish!');
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$product->getId());
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
     }
 }
